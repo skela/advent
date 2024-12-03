0a30a105be79c2d49fc644886b219fe8ced10287
Alek Slater <skela@davincium.com>
2024-12-03 09:31:49 +0100

    puzzle 3 finished

--- src/main.zig
+++ src/main.zig
@@ -1,6 +1,7 @@
 const std = @import("std");
 // pub const puzzle = @import("puzzle1.zig");
-pub const puzzle = @import("puzzle2.zig");
+// pub const puzzle = @import("puzzle2.zig");
+pub const puzzle = @import("puzzle3.zig");
 
 pub fn main() !void {
     try puzzle.puzzle();
---
+++ src/puzzle3.zig
@@ -0,0 +1,110 @@
+const std = @import("std");
+
+fn mul(a: i32, b: i32) i32 {
+    return a * b;
+}
+
+fn trimBefore(input: []const u8, target: u8) ![]const u8 {
+    var index: usize = 0;
+    for (input) |char| {
+        if (char == target) {
+            // Return the slice up to the target character (exclusive)
+            return input[0..index];
+        }
+        index += 1;
+    }
+    return input[0..0];
+}
+
+fn containsSpace(s: []const u8) bool {
+    for (s) |char| {
+        if (char == ' ') {
+            return true;
+        }
+    }
+    return false;
+}
+
+fn isValid(s: []const u8) bool {
+    for (s) |char| {
+        if (!isDigitOrComma(char)) {
+            return false;
+        }
+    }
+    return true;
+}
+
+fn isDigitOrComma(c: u8) bool {
+    return (c >= '0' and c <= '9') or (c == ',');
+}
+
+fn indexOfSubstring(s: []const u8, substring: []const u8) ?usize {
+    return std.mem.indexOf(u8, s, substring);
+}
+
+fn containsSubstring(s: []const u8, substring: []const u8) bool {
+    return indexOfSubstring(s, substring) != null;
+}
+
+pub fn puzzle() !void {
+    const data = @embedFile("puzzle3.data2");
+
+    const split = std.mem.split;
+    var splits = split(u8, data, "mul(");
+
+    const target: u8 = ')';
+    var sum: i32 = 0;
+    var enabled: bool = true;
+    const allocator = std.heap.page_allocator;
+    while (splits.next()) |line| {
+        const trimmed = try trimBefore(line, target);
+
+        if (!isValid(trimmed)) {
+            continue;
+        }
+
+        var comps = split(u8, trimmed, ",");
+
+        var parts = std.ArrayList(i32).init(allocator);
+
+        while (comps.next()) |c| {
+            std.debug.print("trying {s}\n", .{c});
+            const v = std.fmt.parseInt(i32, c, 10) catch |err| {
+                std.debug.print("Skipping invalid value: {s} (error: {any})\n", .{ c, err });
+                continue; // Skip to the next token
+            };
+            try parts.append(v);
+        }
+
+        if (parts.items.len != 2) {
+            continue;
+        }
+
+        const v1 = parts.items[0];
+        const v2 = parts.items[1];
+        if (enabled) {
+            sum += mul(v1, v2);
+        }
+
+        const dont = indexOfSubstring(line, "don't()");
+        const do = indexOfSubstring(line, "do()");
+
+        if (do != null) {
+            if (dont == null) {
+                enabled = true;
+            } else {
+                enabled = do.? > dont.?;
+            }
+        } else if (dont != null) {
+            if (do == null) {
+                enabled = false;
+            } else {
+                enabled = do.? > dont.?;
+            }
+        }
+
+        defer parts.deinit();
+    }
+
+    std.debug.print("Result: {d}\n", .{sum});
+}
